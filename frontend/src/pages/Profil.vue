58e159ae23dea6324acbbf80edf5fcbe4c1cd1ad
Augustin Debureaux <adebureaux@gmail.com>
2022-11-30 14:38:08 +0100

    save

--- frontend/src/pages/Profil.vue
+++ frontend/src/pages/Profil.vue
@@ -1,32 +1,58 @@
 <template>
-    <q-btn
-      @click="loadData"
-      color="primary"
-      label= data.username
-    />
+  <q-page id="home">
+    {{ message }}
+    {{ message }}
+    <q-btn></q-btn>
+  </q-page>
 </template>
 
-<script>
-import { useQuasar } from 'quasar'
-import { ref } from 'vue'
+<script lang="ts">
 import { api } from 'boot/axios'
+import { ref } from 'vue'
+import { defineComponent, PropType } from 'vue'
 
-export default {
-  setup () {
-    const $q = useQuasar()
-    const data = ref(null)
 
-    function loadData () {
+export default defineComponent({
+  name: 'Profile',
+  components: {},
+  el: '#home',
+  props: {
+    name : { type: String, default: "username" },
+  },
+  methods: {
+    loadData : function () {
+      const data = ref(null)
       api.get('/api/users/Matthieu')
         .then((response) => {
           data.value = response.data
-          console.log(data.value)
         })
         .catch(() => {
           console.error('error')
         })
+        console.log(data.value)
+        return (data)
     }
-    return { data, loadData }
-  }
-}
+  },
+  data () {
+    return {
+      message : "test"
+    }
+  },
+  // setup () {
+  //   const $q = useQuasar()
+  //   const data = ref(null)
+
+  //   function loadData () {
+  //     api.get('/api/users/Matthieu')
+  //       .then((response) => {
+  //         data.value = response.data
+  //         console.log(data.value)
+  //       })
+  //       .catch(() => {
+  //         console.error('error')
+  //       })
+  //   }
+  //   return { data, loadData }
+  // }
+})
 </script>
